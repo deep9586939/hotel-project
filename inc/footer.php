620e78eb8ec4cbca7c4b870d39a1c70051e804f8
deep9586939 <141734429+deep9586939@users.noreply.github.com>
2025-07-14 14:32:22 +0530

    Add files via upload

--- inc/footer.php
+++ inc/footer.php
@@ -1,1 +1,37 @@
+    <!--Footer-->
 
+      <div class="container-fluid bg-white mt-5">
+        <div class="row">
+          <div class="col-lg-4 p-4">
+            <h3 class="fw-bold fs-3 mb-2">DH hotel</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+              Debitis, earum. Harum, impedit! Molestiae cupiditate inventore 
+              cum alias tempora unde id, facere magni omnis repellendus aspernatur 
+              sunt dicta distinctio a sit.</p>
+          </div>
+          <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Links</h5>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Home</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Rooms</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Facilities</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">Contact us</a><br>
+            <a href="#" class="d-inline-block mb-2 text-dark text-decoration-none">About</a>
+          </div>
+          <div class="col-lg-4 p-4">
+            <h5 class="mb-3">Follow us</h5>
+            <a href="#" class="d-line-block  text-dark text-decoration-none mb-3">
+              <i class="bi bi-twitter me-1"></i>Twitter
+            </a><br>
+            <a href="#" class="d-line-block  text-dark text-decoration-none mb-3">
+              <i class="bi bi-facebook me-1"></i>Facebook
+            </a><br>
+            <a href="#" class="d-line-block  text-dark text-decoration-none">
+              <i class="bi bi-instagram me-1"></i>Instagram
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <h6 class="text-center bg-dark text-white p-3 m-0">Designed and Developed by Deep hariyani</h6>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>

b8bed5569a359a3fe6a6e5fed65b262e1fe2608a
deep9586939 <141734429+deep9586939@users.noreply.github.com>
2025-07-30 17:44:12 +0530

    Add files via upload

--- admin/settings.php
+++ admin/settings.php
@@ -1,7 +1,7 @@
 <?php 
 
     require('inc/essentials.php');
-    adminlogin();
+    adminLogin();
     
 
 ?>
@@ -22,9 +22,9 @@
             <div class="col-lg-10 ms-auto p-4 overflow-hidden">
                 <h3 class="mb-4">SETTINGS</h3>
 
-                <!--    General settings section-->
+                <!--General settings section-->
 
-                    <div class="card">
+                    <div class="card border-0 shadow-sm mb-4">
                         <div class="card-body">
                             <div class="d-flex align-items-center justify-content-between mb-3">
                                 <h5 class="card-title m-0">General Settings </h5>
@@ -39,7 +39,7 @@
                         </div>
                     </div>
 
-                <!--    General settings modal-->
+                <!--General settings modal-->
 
                     <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
                         <div class="modal-dialog">
@@ -67,6 +67,23 @@
                         </div>
                     </div>
 
+                <!--Shutdown section-->
+
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center justify-content-between mb-3">
+                            <h5 class="card-title m-0">Shutdown Website</h5>
+                            <div class="form-check form-switch">
+                                <form>
+                                    <input onchange="upd_shutdown(this.value)" class="form-check-input" type="checkbox" id="shutdown-toggle">
+                                </form>
+                            </div>
+                        </div>
+                        <p class="card-text">
+                            No customers will be allowed to book hotel room, when shutdown mode is turned on.
+                        </p>
+                    </div>
+                </div>
            </div>
         </div>
     </div>
@@ -83,9 +100,11 @@
         let site_title_inp = document.getElementById('site_title_inp');
         let site_about_inp = document.getElementById('site_about_inp');
 
+        let shutdown_toggle = document.getElementById('shutdown-toggle');
+
         let xhr = new XMLHttpRequest();
         xhr.open("POST","ajax/settings_crud.php",true);
-        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded')
+        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 
         xhr.onload = function(){
             general_data = JSON.parse(this.responseText);
@@ -95,6 +114,15 @@
 
             site_title_inp.value = general_data.site_title;
             site_about_inp.value = general_data.site_about;
+
+            if(general_data.shutdown == 0){
+                shutdown_toggle.checked = false;
+                shutdown_toggle.value = 0;
+            }
+            else{
+                shutdown_toggle.checked = true;
+                shutdown_toggle.value = 1;  
+            }
         }
         xhr.send('get_general');
     }
@@ -103,20 +131,49 @@
     {
         let xhr = new XMLHttpRequest();
         xhr.open("POST","ajax/settings_crud.php",true);
-        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded')
+        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
 
         xhr.onload = function()
         {
-            console.log(this.responseText)
-           /* general_data = JSON.parse(this.responseText);
 
-            site_title.innerText = general_data.site_title;
-            site_about.innerText = general_data.site_about;
+            var myModal = document.getElementById('general-s')
+            var modal = bootstrap.Modal.getInstance(myModal)
+            modal.hide();
 
-            site_title_inp.value = general_data.site_title;
-            site_about_inp.value = general_data.site_about;*/
+            if(this.responseText == 1)
+            {
+                alert('success','Changes saved!');
+                get_general();
+            }
+            else
+            {
+                alert('error','No changes made!');
+            }
+
         }
-        xhr.send('site_title='+site_title_val+'&site_about'+site_about_val+'&upd_general');
+        xhr.send('site_title='+site_title_val+'&site_about='+site_about_val+'&upd_general');
+    }
+
+    function upd_shutdown(val)
+    {
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST","ajax/settings_crud.php",true);
+        xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+
+        xhr.onload = function()
+        {
+            if(this.responseText == 1 && general_data.shutdown==0)
+            {
+                alert('success','Site has been shutdown!');
+            }
+            else
+            {
+                alert('success','Shutdown mode off!');
+            }
+            get_general();
+
+        }
+        xhr.send('upd_shutdown='+val);
     }
     
 

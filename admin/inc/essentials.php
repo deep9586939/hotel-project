b788dd5bf5ced1d9d9252490706790c37b358980
deep9586939 <141734429+deep9586939@users.noreply.github.com>
2025-08-05 12:46:18 +0530

    Add files via upload

--- admin/inc/essentials.php
+++ admin/inc/essentials.php
@@ -40,7 +40,27 @@
         $img_mime = $image['type'];
 
         if(!in_array($img_mime,$valid_mime))
-    }
+        {
+            return 'inv_img'; //invalid image mime or format
+        }
+        else if(($image['size']/(1024*1024))>2)
+        {
+            return 'inv_size'; //invalid size greater than 2mb
+        }
+        else
+        {
+            $ext = pathinfo($image['name'],PATHINFO_EXTENSION);
+            $rname = 'IMG_'.random_int(11111,99999).".$ext";
+            $img_path = UPLOAD_IMAGE_PATH.$folder.$rname;
+            if(move_uploaded_file($image['tmp_name'],$img_path))
+            {
+                return $rname;
+            }
+            else{
+                return 'upd_failed';
+            }
+        }
+}
 
 
 ?>

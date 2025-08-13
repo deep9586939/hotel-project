ee7ed8d41983a0e983c37c6adf323d790da8f6fb
deep9586939 <141734429+deep9586939@users.noreply.github.com>
2025-08-13 21:00:52 +0530

    Add files via upload

--- admin/inc/db_config.php
+++ admin/inc/db_config.php
@@ -11,12 +11,22 @@
         die("Cannot Connect to database".mysqli_connect_error());
     }
 
-    function filteration( $data){
-        foreach($data as $key => $value){
-            $data[$key] = trim($value);
-            $data[$key] = stripcslashes($value);
-            $data[$key] = htmlspecialchars($value);
-            $data[$key] = strip_tags($value);
+    function selectAll($table)
+    {
+        $con = $GLOBALS['con'];
+        $res = mysqli_query($con,"SELECT  * FROM $table");
+        return $res;
+    }
+
+    function filteration( $data)
+    {
+        foreach($data as $key => $value)
+        {
+            $value = trim($value);
+            $value = stripslashes($value);
+            $value = strip_tags($value);
+            $value = htmlspecialchars($value);
+            $data[$key] = $value;
         }
         return $data;
     }
@@ -93,6 +103,31 @@
         else
         {
             die("Query cannot be prepared-Insert");
+        }
+    }
+
+    function delete($sql,$values,$datatypes)
+    {
+        $con = $GLOBALS['con'];
+        if($stmt = mysqli_prepare($con,$sql))
+            {
+                mysqli_stmt_bind_param($stmt,$datatypes,...$values);
+                if(mysqli_stmt_execute($stmt))
+                {
+                    
+                    $res = mysqli_stmt_affected_rows($stmt);
+                    mysqli_stmt_close($stmt);
+                    return $res;
+                }
+                else
+                {
+                    mysqli_stmt_close($stmt);
+                    die("Query cannot be executed-Delete");
+                }
+            }
+        else
+        {
+            die("Query cannot be prepared-Delete");
         }
     }
     
